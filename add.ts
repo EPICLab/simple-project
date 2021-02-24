0b7812d6fe42c4b2004b81ddb9d2a016eb3db910
Nicholas Nelson <nelsonni@oregonstate.edu>
2021-02-23 21:09:39 -0800

    added up to c

--- add.ts
+++ add.ts
@@ -1,7 +1,7 @@
 function addNumbers(a: number, b: number) {
-  return a + b;
+  return a + b + c;
 }
 
-let sum: number = addNumbers(10, 15);
+let sum: number = addNumbers(10, 12);
 
 console.log('Sum of two numbers is: ' + sum);
